b8b35c81413d3406633e458e4346b6dba6309a39
Olya <ola.shy@yahoo.com>
2021-06-16 10:49:11 +0300

    add functions

--- script.js
+++ script.js
@@ -9,24 +9,40 @@ const personalMovieDB = {
     privat: false,
 };
 
-let i = 1;
-let a, b;
-while (i <= 2) {
-    a = prompt('Один из последних просмотренных фильмов?');
-    b = prompt('На сколько оцените его?');
-    i++;
-    if (a === '' || a.length > 50) {
-        continue;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
     }
 }
 
-personalMovieDB.movies[a] = b;
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count < 30) {
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
+} else if (personalMovieDB.count >= 30) {
     console.log('Вы киноман');
 } else {
     console.log('Произошла ошибка');
+}
+
+/* 2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы*/
+function showMyDB() {
+    if (privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+/* 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных genres*/
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        let userAnswr = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+    genres.push(userAnswr);
 }
