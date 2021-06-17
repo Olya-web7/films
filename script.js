1902f22ac2da60fdca22e4ffda1fad169fed8660
Olya <ola.shy@yahoo.com>
2021-06-17 15:21:27 +0300

    obj

--- script.js
+++ script.js
@@ -10,54 +10,58 @@ function start() {
 start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false,
+        toggleVisibleMyDB: function() {
+            if (privat === false) {
+                privat == true;
+            }
+        },
+        showMyDB: function(hidden) {
+            if (!hidden) {
+                console.log(personalMovieDB);
+            }
+        },
+        detectPersonalLevel: function() {
+            if (personalMovieDB.count < 10) {
+                console.log('Просмотрено довольно мало фильмов');
+            } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+                console.log('Вы классический зритель');
+            } else if (personalMovieDB.count >= 30) {
+                console.log('Вы киноман');
+            } else {
+                console.log('Произошла ошибка');
+            }
+        },
+        rememberMyFilms: function() {
+            for (let i = 0; i < 2; i++) {
+                const a = prompt('Один из последних просмотренных фильмов?', ''),
+                    b = prompt('На сколько оцените его?', '');
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
+                if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                } else {
+                    i--;
+                }
+            }
+        },
+        writeYourGenres: function() {
+            for (let i = 1; i <= 3; i++) {
 
-        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            i--;
-        }
+                personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            }
+            if (personalMovieDB.genres[i - 1] == null || personalMovieDB.genres[i - 1] == '') {
+                i--;
+            }
+            personalMovieDB.genres.forEach(function(item, i) {
+                console.log(`Любимый жанр ${i} - это ${personalMovieDB.genres[i]}`);
+            })
+        },
     }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
+    /* 
+    
+    при помощи метода forEach вывести в консоль сообщения в таком виде:
+    ""*/
